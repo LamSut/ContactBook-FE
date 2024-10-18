ab5cdcb6cf52de6be2c843ced244591b52808f62
LamSut <lamsut0007@gmail.com>
2024-10-18 22:40:45 +0700

    tanstack plugin

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import router from './router';
 
 import { createApp } from 'vue';
 import App from './App.vue';
+import { VueQueryPlugin } from '@tanstack/vue-query';
 
 createApp(App)
+    .use(VueQueryPlugin)
     .use(router)
     .mount('#app');
